1284c93802cec8075614891240559565b033221e
ceylanmesut <mesutceylan95@gmail.com>
2023-03-16 16:53:23 +0100

    bug fixs and documentation

--- main.py
+++ main.py
@@ -1,68 +1,59 @@
 
 #%%
 
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 from gensim.models import LdaModel
-from gensim.corpora.dictionary import Dictionary
-
-from gensim import models, corpora, similarities
+from gensim import models, corpora
 import nltk
-from nltk import FreqDist
 
-from sklearn.metrics.pairwise import cosine_similarity
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.stats import entropy
 
 import pandas as pd
 import utils
 
-
-nltk.download('stopwords')
+# nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words('english')
-
-#%%
-experiment = None
-
 
 data = pd.read_csv("data\\co_data.csv")
 
 # arguments
 CO_NAME = "Vahanalytics"
 TOP_K = 5
-NUM_TOPICS = 10
+NUM_TOPICS = 4
 CHUNK = 5
-TOKEN_SIZE = 5000 
+TOKEN_SIZE = 15000 
 experiment = "LDA"
 
 if experiment == "TF-IDF":
     
     tfidf = TfidfVectorizer(utils.prep_process_tokenize).fit_transform(data["description"])
-    query_index = utils.query_company(data, CO_NAME)
-    similar_co_ind = utils.query_sim(tfidf, query_index, TOP_K)
-    tfidf_result = utils.get_result(data, similar_co_ind)
+    query_index = utils.query_idx(data, CO_NAME)
+    similar_co_ind, cosine_sims = utils.query_sim(tfidf, query_index, TOP_K)
+    tfidf_result = utils.get_result(experiment, data, similar_co_ind, TOP_K)
  
 elif experiment == "LDA":
     
     data_lda = utils.build_tokenized_col(data, stopwords)
-    most_common_tokens = utils.most_tokens(data_lda, TOKEN_SIZE)
-    training_set = utils.get_training_data(most_common_tokens, data_lda)
+    training_set = utils.get_training_data(data_lda)
     
     dictionary = corpora.Dictionary(training_set)
-    # dictionary = corpora.Dictionary(training_set["tokenized"])
-    corpus = [dictionary.doc2bow(doc) for doc in training_set]
-    lda = LdaModel(corpus=corpus, num_topics=NUM_TOPICS, id2word=dictionary, alpha=1e-2, eta=0.5e-2, chunksize=CHUNK)
+    corpus = utils.get_corpus(training_set, dictionary)
+    lda = LdaModel(corpus=corpus, num_topics=NUM_TOPICS, id2word=dictionary, alpha=0.0001, chunksize=CHUNK, random_state=42)
     
-    docmat_pad = utils.doc_matrix(lda, corpus, NUM_TOPICS)
-    query_index = utils.query_company(data, CO_NAME)
+    docmat_pad, docmat = utils.doc_matrix(lda, corpus, NUM_TOPICS)
+    query_index = utils.query_idx(data, CO_NAME)
     new_doc_mat = utils.get_dict2bow(data, lda, dictionary, query_index)
     query_answer = utils.matpad([new_doc_mat], NUM_TOPICS)[0]
     
+    most_sim_ids, sims = utils.get_top_k_similar_docs(query_answer, docmat_pad, 2000, entropy, k=5)
+    lda_result = utils.get_result(experiment, data, most_sim_ids, TOP_K)
     
-    print("QUERY ANS", query_answer)
-    print("QUERY.ANS.SHAPE", query_answer.shape)
-    input()    
-    
-    most_sim_ids = utils.get_top_k_similar_docs(query_answer, docmat_pad, 2000, entropy, k=5)
-    lda_result = utils.get_result(data, most_sim_ids)
+
  
 # %%
+
+coherence_model_lda = models.CoherenceModel(model=lda, texts=training_set, dictionary=dictionary, coherence="u_mass")
+coherence_lda = coherence_model_lda.get_coherence()
+print('\nCoherence Score: ', coherence_lda)
--- utils.py
+++ utils.py
@@ -5,10 +5,9 @@ import nltk
 
 import numpy as np
 import pandas as pd
-from  collections import Counter
+# from  collections import Counter
 
 from sklearn.metrics.pairwise import cosine_similarity
-from scipy.stats import entropy
 
 nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words('english')
@@ -16,36 +15,70 @@ stopwords = nltk.corpus.stopwords.words('english')
 
 def prep_process_tokenize(text, stopwords):
     
-    #websites, email and any punctuation cleaning
+    """Text cleaner.
+    Args:
+        text: dataframe, the company data
+        stopwords: list, english stopwords
+    Returns:
+        text: lower-cased, tokenized and processed text 
+    """
+    
+    # cleaning the URLs, email and any punctuation existing
     text = re.sub("((\S+)?(http(s)?)(\S+))|((\S+)?(www)(\S+))|((\S+)?(\@)(\S+)?)", " ", text)
     text = re.sub("[^a-zA-Z ]", "", text)
-    text = text.lower() # lower case the text
+    text = text.lower() # lower casing the text
     text = nltk.word_tokenize(text) # tokenizing
     
-    #removing stopwords
+    # removing stopwords
     text = [word for word in text if word not in stopwords]
     
     return text
 
-
 def matpad(docmat, num_topics):
-  stub_mat = []
-  for doc in docmat:
-    present = set(map(lambda x: x[0], doc))
-    if present == num_topics:
-      stub_mat.append(doc)
-      continue
-    topic_sum = sum(map(lambda x: x[1], doc))
-    reminders = (1 - topic_sum) / (num_topics - len(present))
-    d_doc = dict(doc)
-    stub_mat.append([d_doc[i] if i in present else reminders for i in range(num_topics)])
     
-  return np.asarray(stub_mat)
+    """Document matrix padder.
+    Args:
+        docmat: array, document matrix
+        num_topics: int, number of topics
+    Returns:
+        stub_mat: array, padded matrix
+    """
+    
+    stub_mat = []
+    for doc in docmat:
+        present = set(map(lambda x: x[0], doc))
+        if present == num_topics:
+            stub_mat.append(doc)
+            continue
+  
+        topic_sum = sum(map(lambda x: x[1], doc))
+        reminders = (1 - topic_sum) / (num_topics - len(present)) # TODO: zero encountered in scalar divide
+        d_doc = dict(doc)
+        stub_mat.append([d_doc[i] if i in present else reminders for i in range(num_topics)])
+        
+    return np.asarray(stub_mat)
 
 def pre_process(text, stopwords):
+    
+    """Text preprocessor.
+    Args:
+        text: dataframe, the company data
+        stopwords: list, english stopwords
+    Returns:
+        text: lower-cased, tokenized and processed text 
+    """
+    
     return " ".join(prep_process_tokenize(text,stopwords))
 
 def build_tokenized_col(data, stopwords):
+    
+    """Builds token column.
+    Args:
+        data: dataframe, the company data
+        stopwords: list, english stopwords
+    Returns:
+        data: dataframe with token column
+    """        
     
     data["tokenized"] = np.nan
     for index in (range(len(data["description"]))):
@@ -53,92 +86,161 @@ def build_tokenized_col(data, stopwords):
 
     return(data)
 
-def most_tokens(data, top_n=5000):
+def get_training_data(data):
     
-    tokens_list = []
-
-    for index in range(len(data["tokenized"])):
-        tokens = data["tokenized"][index].split()
-        
-        for token in tokens:
-            tokens_list.append(token)
-            
-    counted_tokens = Counter(tokens_list)
-    most_common_tokens = counted_tokens.most_common(top_n)
-
-    most_common_tokens = []
-    for index in range(len(most_common_tokens)):
-        most_common_tokens.append(most_common_tokens[index][0]) 
-
-    return(most_common_tokens)
-
-def get_training_data(common_tokens, data):
-    # Filtering uncommon words from column values
-    most_common_5000_tokens_set  = set(common_tokens)
+    """Builds training data.
+    Args:
+        data: dataframe, the company data
+    Returns:
+        data: pandas series, tokens
+    """    
 
     for index in range(len(data["tokenized"])):
 
         # splitting the tokens in each row of df
         tokens = data["tokenized"][index].split()
-        #tokens = set(tokens)
         
-        #filtered_tokens = most_common_5000_tokens_set & tokens
-
         # replacing column values with the new column values
-        data["tokenized"][index] = list(tokens)#(filtered_tokens)
-        
-        data_to_train = data["tokenized"] #.astype("string")                  
+        data["tokenized"][index] = list(tokens)
+        data_to_train = data["tokenized"]
     
     return(data_to_train)
 
+def get_corpus(data, dictionary):
+    
+    """Builds corpus for LDA model.
+    Args:
+        data: dataframe, the company data
+        dictionary: gensim dict obj, dictionary of training set 
+        query_idx: int, query caompany index
+    Returns:
+        corpus: tuple, word_id and word frequency
+    """      
+    
+    corpus = [dictionary.doc2bow(doc) for doc in data]    
+    return corpus
 
 def get_dict2bow(data, lda, dictionary, query_idx):
     
-    print("QUERY_IDX", type(query_idx))
-    print("DATA CORR", data.loc[query_idx]["tokenized"])
-    new_bow_Vahanalytics = dictionary.doc2bow(data.loc[query_idx]["tokenized"])
-    new_doc_Vahanalytics = lda.get_document_topics(bow=new_bow_Vahanalytics)    
+    """Builds bag of words from gensim dictionary.
+    Args:
+        data: dataframe, the company data
+        lda: gensim lda obj, trained lda model 
+        dictionary: gensim dict obj, dictionary of training set 
+        query_idx: int, query caompany index
+    Returns:
+        topic_dist: tuple, topic distribution: topic_id, topic probability 
+    """    
+    # turning dictionary to bag of words of query company
+    query_bow = dictionary.doc2bow(data.loc[query_idx]["tokenized"])
+    # obtaining topic distribution of the query company
+    topic_dist = lda.get_document_topics(bow=query_bow)
     
-    return(new_doc_Vahanalytics)
+    return(topic_dist)
     
 def doc_matrix(trained_model, corpus, num_topics):
+    
+    """Builds document matrix.
+    Args:
+        trained_model: gensim lda obj, trained LDA model
+        corpus: array, word_id and word frequency
+        num_topics: int, number of topics
+    Returns:
+        docmat_pad: array, padded document matrix, shape n x num.of.topics
+    """       
     
     docmat = ([x for x in trained_model.get_document_topics(trained_model[corpus])])
     docmat_pad = matpad(docmat, num_topics)
     
-    return(docmat_pad)
+    return(docmat_pad, docmat)
 
 def jensen_shannon(query, matrix, num_documents, entropy):
     
+    """Calculates Jensen-Shannon Divergence.
+    Args:
+        query: array, query answer from LDA
+        matrix: array, word_id and word frequency
+        num_documents: int, number of documents
+        entropy: scipy.stats obj, entropy 
+    Returns:
+        js_divergence: computed js_div scores
+    """       
+    
     p = np.matrix([query for i in range(num_documents)]).T
     q = matrix.T 
-    m = 0.5*(p + q)
+    m = 0.5 * (p + q)
     
     return np.sqrt(0.5*(entropy(p, m) + entropy(q, m)))
 
 def get_top_k_similar_docs(query, matrix, num_documents, entropy, k=10):
     
+    """Gets Top k similar documents.
+    Args:
+        query: array, query answer from LDA
+        matrix: array, word_id and word frequency
+        num_documents: int, number of documents
+        entropy: scipy.stats obj, entropy
+        k: int,  top k similarity threshold
+    Returns:
+        similar_documents: top k similar documents
+    """       
+    
     sims = jensen_shannon(query, matrix, num_documents, entropy)
     
-    return sims.argsort()[:k]
+    return(sims.argsort()[:k], sims)
 
 
-def query_company(data, co_name):
+def query_idx(data, co_name):
+    
+    """Fetches the index of the query company name.
+    Args:
+        data: dataframe, the company data
+        co_name: str, name of the query company, e.g:"Vahanalytics"
+    Returns:
+        index: int, query company index
+    """
     
     co_index = data.index[data['name'] == co_name].tolist()[0]    
     return co_index
 
 def query_sim(tf_idf, query_index, top_k):
     
-    cosine_similarities = cosine_similarity(tf_idf[query_index: query_index + 1], tf_idf).flatten()
-    related_docs_indices = cosine_similarities.argsort()[:-top_k - 1:-1]   
-
-    return(related_docs_indices)
-
-def get_result(data, sim_ind):
+    """Cosine similarity calculator.
+    Args:
+        tf_idf: tf-idf object, fitted and transformed tf-idf object
+        query_index: int, query company index
+        top_k: int, top k similarity threshold
+    Returns:
+        related_docs_ind: array, indicies of the top similar documents 
+        cos_sim: array, cosine similarities
+    """    
     
-    result_df = data[data.index.isin(sim_ind)]  
+    cos_sim = cosine_similarity(tf_idf[query_index: query_index + 1], tf_idf).flatten()
+    related_docs_ind = cos_sim.argsort()[:-top_k - 1:-1]   
 
-    return result_df 
+    return(related_docs_ind, cos_sim)
+
+def get_result(experiment, data, sim_ind, top_k):
+    
+    """Builds result dataframe.
+    Args:
+        experiment: str, name of the experiment
+        data: dataframe, the company data
+        sim_ind: array, indicies of the top similar documents 
+        top_k: int, top k similarirty threshold
+    Returns:
+        result: dataframe, contains top k similar company descriptions and names
+    """    
+    
+    result = pd.DataFrame(columns=["name", "description"])
+
+    for ind in sim_ind:
+        answer = data[["name", "description"]][data.index==ind]
+        result = pd.concat([result, answer])
+    
+    if experiment == "TF-IDF": 
+        result["Cosine Similarities"] = sorted(sim_ind, reverse=True)[: top_k]
+
+    return result 
     
      
